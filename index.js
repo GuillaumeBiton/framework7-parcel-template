361edc4714a5b1730b336eb902834f2a55a96056
Guillaume Biton <guillaume.biton@free.fr>
2017-12-22 11:35:37 +0100

    iPhone X fix

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Librairies
 import Framework7 from 'framework7'
 
+// Init F7
 var app = new Framework7({
   root: '#app',
+  // Fix for iPhone X notch
+  statusbar: {
+    overlay: Framework7.device.ios ? Framework7.device.webView || Framework7.device.cordove : 'auto',
+  },
 })
