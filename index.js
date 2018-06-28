1e537730dc2af68cf1c581269d44364a882e6d37
Guillaume Biton <guillaume.biton@free.fr>
2018-06-28 12:36:52 +0200

    add theme

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Librairies
 import Framework7 from 'framework7/framework7.esm'
 
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
 // Init F7
 var app = new Framework7({
   root: '#app',
+  theme: theme,
   // Fix for iPhone X notch
   statusbar: {
     overlay: Framework7.device.ios ? Framework7.device.webView || Framework7.device.cordova : 'auto',
