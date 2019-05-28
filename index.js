1af2cbc1abb77013763a85f03672e97a26afe122
Guillaume Biton <guillaume.biton@free.fr>
2019-05-28 22:25:01 +0200

    check undefined theme

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 import Framework7 from 'framework7/framework7.esm'
 
 // Theme
-var theme = 'auto';
-if (localStorage.getItem('theme')) {
+var theme = localStorage.getItem('theme');
+if (typeof test !== "undefined" && test !== null) {
   theme = JSON.parse(localStorage.getItem('theme'));
+} else {
+  theme =  'auto';
 }
 
 // Init F7
